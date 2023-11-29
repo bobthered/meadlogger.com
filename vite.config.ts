e2cdf3b67f4a1e5abc0b19f00fabac57af874ce0
Bob McAleavey <bobmcaleavey@gmail.com>
2023-11-29 08:47:49 -0500

    feat(alias): add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$actions: resolve('./src/actions'),
+			$components: resolve('./src/components'),
+			$icons: resolve('./src/icons'),
+			$stores: resolve('./src/stores')
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
