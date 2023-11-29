79ef53fa2bf6d5d0017ce205e515dfe7a4d2130d
Bob McAleavey <bobmcaleavey@gmail.com>
2023-11-29 08:08:41 -0500

    feat(tailwind palette): add tailwindcss-palette-generator

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,22 @@
+const { tailwindcssPaletteGenerator } = require("@bobthered/tailwindcss-palette-generator");
+
 /** @type {import('tailwindcss').Config}*/
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: tailwindcssPaletteGenerator({
+				colors: [
+					'#e5d524',
+					'#898661'
+				],
+				names: [
+					'violet',
+					'slate'
+				]
+			})
+		}
 	},
 
 	plugins: []
